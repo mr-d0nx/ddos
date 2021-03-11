420268c5369fb1fdfc0ceaf1d71b181440020841
mr-d0nx <76074196+mr-d0nx@users.noreply.github.com>
2021-03-11 07:08:40 +0700

    Create tools.sh

---
+++ tools.sh
@@ -0,0 +1,180 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+echo $green
+figlet "ddos"
+echo
+echo $green" +----------------------+"
+echo $green" + Author : Mr D0nx     +"
+echo $green" + Team   : IndoDarknet +"
+echo $green" + IG     : Moh.dito_   +"
+echo $green" +----------------------+"
+echo
+sleep 1
+echo $green" Menu :"
+echo
+echo $green" [1] DDOS NO TELEPON"
+echo
+echo $green" [2] DDOS IP ADDRES"
+echo
+echo $green" [3] DDOS Link Website"
+echo
+echo $green" [0] Keluar Tools?"
+sleep 1
+echo
+echo $green" Pilih Sesuai Nomor"
+echo $green
+read -p " ------------>  " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $green
+figlet "DDOS"
+sleep 1
+echo $green"By MrD0nx"
+read -p "Masukan No HP : " telp
+read -p "Masukan Jumlah: " jmlh
+sleep 1
+echo $green"MENGIRIM...."
+echo $green
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo "Mengirim Virus Ke Nomor $telp"
+sleep 1
+echo
+echo $green"Done.."
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $green
+figlet "DDOS"
+sleep 1
+echo $green"By MrD0nx"
+echo
+sleep 1
+read -p "Masukan Ip : " ip
+read -p "Masukan Port :" port
+read -p "Masukan Jumlah :" banyak
+sleep 1
+echo $gree
+echo "Mengirim.... "
+echo
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+sleep 1
+echo
+echo $green"Berhasil Mengirim $banyak Port $port to $ip
+echo
+echo $green"Done...."
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $green
+figlet "DDOS"
+echo $green"By MrD0nx"
+sleep 1
+read -p "Masukan Link : " link
+read -p "Masukan Jumlah/detik : " js
+sleep 1
+echo
+echo $green"Mengirim..... "
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+sleep 1
+echo
+echo $green"ping port attack to $link set $js"
+echo
+echo $green"Done.... "
+fi
+
+if [ $bro = 0 ] || [ $bro = 0 ]
+then
+echo $green
+figlet "MrD0nx"
+sleep 1
+echo $green"Terimakasih Telah Menggunakan Tools ;)"
+sleep 1
+echo $green"Tools by MrD0nx"
+sleep 1
+exit
